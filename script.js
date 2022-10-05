f26ef0569b7606d560d6f92170c3ab948427856a
Christopher Shaw <silvertn350@yahoo.com>
2022-10-05 15:04:33 -0500

    Progression Update 2

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//----------------------------------------------------------------Background Canvas
+const collisionCanvas = document.getElementById('collisionCanvas');
+const collisionCtx = collisionCanvas.getContext('2d');
+collisionCanvas.width = window.innerWidth;
+collisionCanvas.height = window.innerHeight;
+
+//-----------------------------------------------------------------Raven Canvas
+
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
@@ -7,13 +15,18 @@ canvas.height = window.innerHeight;
 let timeToNextRaven = 0;
 let ravenInterval = 500;
 let lastTime = 0;
+let score = 0;
+ctx.font = '50px Impact';
 
 let ravens = [];
 //----------------------------------------------------------------Class Raven
 class Raven {
     constructor(){
-        this.width = 100;
-        this.height = 100;
+        this.spriteWidth = 271; //overall width of sprite sheet divided by amount of sprites on the sheet // width = 1626 /6 sprites = 271px
+        this.spriteHeight = 194; //total height of the sprite sheet
+        this.sizeModifier = Math.random() * 0.6 + 0.4;
+        this.width = this.spriteWidth * this.sizeModifier;
+        this.height = this.spriteHeight * this.sizeModifier;
         this.x = canvas.width;
         this.y = Math.random() * (canvas.height - this.height);
         this.directionX = Math.random() * 5 + 3;
@@ -21,23 +34,66 @@ class Raven {
         this.markedForDeletion = false;
         this.image = new Image();
         this.image.src = '/img/raven.png';
-        this.spriteWidth = 271; //overall width of sprite sheet divided by amount of sprites on the sheet. width = 1626 /6 sprites = 271
-        this.spriteHeight = 194; //total height of the sprite sheet
-
-
+        this.frame = 0;
+        this.maxFrame = 4;
+        this.timeSinceFlap = 0;
+        this.flapInterval = Math.random() * 50 + 50;
+        this.randomColors = [Math.floor
+                            (Math.random() * 255),              //Green
+                            Math.floor(Math.random() * 255),    //Red
+                            Math.floor(Math.random() * 255)];   //Blue
+        this.color = 'rgb(' + this.randomColors[0] + ',' + this.randomColors[1]
+        + ',' +this.randomColors[2] + ')';
     }
 //----------------------------------------------------------------Update Function
-    update() {
+    update(deltatime) {
+        if (this.y < 0 || this.y > canvas.height - this.height){
+            this.directionY = this.directionY * -1;
+        }
         this.x -= this.directionX;
+        this.y += this.directionY;
         if (this.x < 0 - this.width) this.markedForDeletion = true;
+        this.timeSinceFlap += deltatime;
+        if ( this.timeSinceFlap > this.flapInterval){
+        if (this.frame > this.maxFrame) this.frame = 0;
+        else this.frame++;
+        this.timeSinceFlap = 0;
     }
+}
+
 //----------------------------------------------------------------Draw Function
     draw() {
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, 0, 0,this.spriteWidth,this.spriteHeight, this.x, this.y,this.width, this.height);
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(
+            this.image, 
+            this.frame * this.spriteWidth,
+            0,
+            this.spriteWidth,
+            this.spriteHeight,
+            this.x,
+            this.y,
+            this.width,
+            this.height);
         this.height;
     }
 }
+
+//----------------------------------------------------------------ScoreFunction
+
+function drawScore(){
+    ctx.fillStyle = 'black';
+    ctx.fillText('Score:' + score, 50,75);
+    ctx.fillStyle = 'white';
+    ctx.fillText('Score:' + score, 55,80);
+    }
+
+//----------------------------------------------------------------Event Listener
+
+window.addEventListener('click', function(e){
+    const detectPixelColor = ctx.getImageData(e.x,e.y,1,1);
+    console.log(detectPixelColor);
+})
 
 
 //----------------------------------------------------------------Animate Function
@@ -50,9 +106,10 @@ function animate(timestamp){
         ravens.push(new Raven());
         timeToNextRaven = 0;
     };
+    drawScore();
     //array literal // spread operator
     //cycles through entire array and triggers update
-    [...ravens].forEach(object => object.update());
+    [...ravens].forEach(object => object.update(deltatime));
     [...ravens].forEach(object => object.draw());
     ravens = ravens.filter(object => !object.markedForDeletion)
     requestAnimationFrame(animate);
